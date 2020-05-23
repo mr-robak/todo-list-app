681b07338fe8b632b9756c78f27cec66c1682d14
Marcin Robak <mr.robak@gmail.com>
2020-05-23 16:41:43 +0200

    db create,models created + migrate

---
+++ models/user.js
@@ -0,0 +1,20 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      name: DataTypes.STRING,
+      email: {
+        allowNull: false,
+        unique: true,
+        type: DataTypes.STRING,
+      },
+      phone: DataTypes.INTEGER,
+    },
+    {}
+  );
+  User.associate = function (models) {
+    // associations can be defined here
+  };
+  return User;
+};
